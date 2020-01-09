eea0736296bf25d6410b1407c113ad43f37e0055
BuckarooBanzay <BuckarooBanzay@github.com>
2020-01-09 09:53:07 +0100

    local test setup

--- test/start.sh
+++ test/start.sh
@@ -4,4 +4,5 @@ docker run --rm -it \
 	-u root:root \
 	-v $(pwd)/minetest.conf:/etc/minetest/minetest.conf \
 	-v $(pwd)/../:/root/.minetest/worlds/world/worldmods/mail_mod \
-	registry.gitlab.com/minetest/minetest/server:5.0.1
+	--network host \
+	registry.gitlab.com/minetest/minetest/server:5.1.0
