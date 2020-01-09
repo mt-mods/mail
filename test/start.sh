c094a4e1a2263d6c9e28279cafb03b827ec5a62d
BuckarooBanzay <BuckarooBanzay@github.com>
2020-01-09 09:40:01 +0100

    fix permission issue in test deployment

--- test/start.sh
+++ test/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker run --rm -it \
+	-u root:root \
 	-v $(pwd)/minetest.conf:/etc/minetest/minetest.conf \
-	-v /tmp/mt:/var/lib/minetest/.minetest \
-	-v $(pwd)/../:/var/lib/minetest/.minetest/worlds/world/worldmods/mail_mod \
+	-v $(pwd)/../:/root/.minetest/worlds/world/worldmods/mail_mod \
 	registry.gitlab.com/minetest/minetest/server:5.0.1
