68a5d61b5dc10b0f0bd0856e35b1900fc6d4bb46
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-03-26 10:46:02 +0100

    persistent world

--- test/start.sh
+++ test/start.sh
@@ -4,5 +4,6 @@ docker run --rm -it \
 	-u root:root \
 	-v $(pwd)/minetest.conf:/etc/minetest/minetest.conf \
 	-v $(pwd)/../:/root/.minetest/worlds/world/worldmods/mail_mod \
+	-v mail_world:/root/.minetest/worlds/world/ \
 	--network host \
 	registry.gitlab.com/minetest/minetest/server:5.1.0
