be51cff470d63c2bea1d3f568ed8f6664db64032
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-09-20 13:11:38 +0200

    field fix

--- webmail/handler_send.lua
+++ webmail/handler_send.lua
@@ -2,6 +2,6 @@
 -- send request from webmail
 function mail.handlers.send(sendmail)
 	-- send mail from webclient
-	minetest.log("action", "[webmail] sending mail from webclient: " .. sendmail.src .. " -> " .. sendmail.dst)
+	minetest.log("action", "[webmail] sending mail from webclient: " .. sendmail.from .. " -> " .. sendmail.to)
 	mail.send(sendmail)
 end
