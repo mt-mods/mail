f53221e742a2a9f826c3271109b3e1d33c847df6
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-02-23 23:31:01 +0100

    fix smoke test

--- mtt.lua
+++ mtt.lua
@@ -1,5 +1,9 @@
 
 mtt.register("send mail", function(callback)
+    -- create "player2"
+    local auth_handler = minetest.get_auth_handler()
+    auth_handler.set_password("player2", "")
+
     -- send a mail
     mail.send("player1", "player2", "something", "blah")
 
