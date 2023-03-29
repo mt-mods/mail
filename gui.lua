9cbe7aa22b54246f69edb4c3dd26930140427dfe
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-29 17:23:35 +0200

    unified-inv fix

--- gui.lua
+++ gui.lua
@@ -81,6 +81,9 @@ if minetest.get_modpath("unified_inventory") then
 	unified_inventory.register_button("mail", {
 			type = "image",
 			image = "mail_button.png",
-			tooltip = "Mail"
+			tooltip = "Mail",
+			action = function(player)
+				mail.show_mail_menu(player:get_player_name())
+			end
 		})
 end
