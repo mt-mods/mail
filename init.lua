ed3c8b97a1a9a8b2519953696843be2ebdd9d1bd
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2023-04-18 22:01:24 +0200

    update testing harness / move contact-list function to util (#73)
    * update testing harness / move contact-list function to util
    
    * Use 5.x.1 versioning
    
    ---------
    
    Co-authored-by: BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
    Co-authored-by: Athozus <athozus@gmail.com>

--- init.lua
+++ init.lua
@@ -38,6 +38,7 @@ end
 -- sub files
 local MP = minetest.get_modpath(minetest.get_current_modname())
 dofile(MP .. "/util/normalize.lua")
+dofile(MP .. "/util/contact.lua")
 dofile(MP .. "/util/uuid.lua")
 dofile(MP .. "/chatcommands.lua")
 dofile(MP .. "/migrate.lua")
