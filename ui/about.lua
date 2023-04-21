1a84c66346375eb34a4b9bf5d961cae94d3761d4
Athozus <athozus@gmail.com>
2023-04-21 23:33:21 +0200

    Update about.lua

--- ui/about.lua
+++ ui/about.lua
@@ -6,7 +6,7 @@ function mail.show_about(name)
 			button[9.35,0;0.75,0.5;back;X]
 			label[0,0;Mail]
 			label[0,0.4;Provided my mt-mods]
-			label[0,0.8;Version: 1.1.1]
+			label[0,0.8;Version: 1.1.3]
 			label[0,1.4;Licenses:]
 			label[0.2,1.8;Expat (code), WTFPL (textures)]
 			label[0,2.4;https://github.com/mt-mods/mail]
@@ -24,8 +24,13 @@ function mail.show_about(name)
 			[[#FFF,Athozus,Outbox\, Maillists\, UI\, Drafts,]] ..
 			[[#FFF,fluxionary,Minor fixups,]] ..
 			[[#FFF,SX,Various fixes\, UI,]] ..
-			[[#FFF,Toby1710,Ux fixes,]] ..
-			[[#FFF,Peter Nerlich,CC\, BCC]
+			[[#FFF,Toby1710,UX fixes,]] ..
+			[[#FFF,Peter Nerlich,CC\, BCC,]] ..
+			[[#FFF,Niklp,German translation,]] ..
+			[[#FFF,Emojigit,Chinese translation,]] ..
+			[[#FFF,Dennis Jenkins,UX fixes,]] ..
+			[[#FFF,Thomas Rudin,Maintenance,]] ..
+			[[#FFF,NatureFreshMilk,Maintenance]
 		]] .. mail.theme
 
 	minetest.show_formspec(name, FORMNAME, formspec)
