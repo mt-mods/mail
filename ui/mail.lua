d43955c6c0878566f4b7bdad44ccbefd1a8bbbd8
Athozus <athozus@gmail.com>
2023-04-26 11:05:09 +0200

    Give a default boxtab value (fix trivial issues)

--- ui/mail.lua
+++ ui/mail.lua
@@ -2,6 +2,9 @@
 
 function mail.show_mail_menu(playername, sortfield, sortdirection, filter)
     local index = mail.selected_idxs.boxtab[playername] or 1
+    if not mail.selected_idxs.boxtab[playername] then
+        mail.selected_idxs.boxtab[playername] = 1
+    end
     if index == 1 then
         mail.show_inbox(playername, sortfield, sortdirection, filter)
     elseif index == 2 then
