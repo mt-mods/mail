8dadd8043ce22a53e161ab379998d7bf3a509036
Athozus <athozus@gmail.com>
2023-06-18 15:29:27 +0200

    Permit start composition of a message by passing receivers into /mail command (close #101)

--- chatcommands.lua
+++ chatcommands.lua
@@ -1,6 +1,10 @@
 minetest.register_chatcommand("mail",{
 	description = "Open the mail interface",
-	func = function(name)
-		mail.show_mail_menu(name)
+	func = function(name, param)
+		if #param > 0 then -- if param is not empty
+			mail.show_compose(name, param) -- make a new message
+		else
+			mail.show_mail_menu(name) -- show main menu
+		end
 	end
 })
