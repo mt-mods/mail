7ae5bda5ab0658b6cd11b289bdfa44acf9e61ba7
Athozus <athozus@gmail.com>
2023-07-20 00:33:59 +0200

    Quick restructuration of files (create sub-init.lua)

--- ui/init.lua
+++ ui/init.lua
@@ -1,3 +1,23 @@
+-- sub files
+local MP = minetest.get_modpath(minetest.get_current_modname())
+
+dofile(MP .. "/ui/inbox.lua")
+dofile(MP .. "/ui/outbox.lua")
+dofile(MP .. "/ui/drafts.lua")
+dofile(MP .. "/ui/trash.lua")
+dofile(MP .. "/ui/message.lua")
+dofile(MP .. "/ui/receivers.lua")
+dofile(MP .. "/ui/events.lua")
+dofile(MP .. "/ui/contacts.lua")
+dofile(MP .. "/ui/edit_contact.lua")
+dofile(MP .. "/ui/select_contact.lua")
+dofile(MP .. "/ui/maillists.lua")
+dofile(MP .. "/ui/edit_maillists.lua")
+dofile(MP .. "/ui/compose.lua")
+dofile(MP .. "/ui/options.lua")
+dofile(MP .. "/ui/settings.lua")
+dofile(MP .. "/ui/about.lua")
+
 -- helper function for tabbed overview
 
 function mail.show_mail_menu(playername, sortfield, sortdirection, filter)
---
+++ util/init.lua
@@ -0,0 +1,6 @@
+-- sub files
+local MP = minetest.get_modpath(minetest.get_current_modname())
+dofile(MP .. "/util/normalize.lua")
+dofile(MP .. "/util/contact.lua")
+dofile(MP .. "/util/uuid.lua")
+dofile(MP .. "/util/time_ago.lua")
