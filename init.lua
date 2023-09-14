e5996469fbb1bc079c3873ba1cade1c432c271f7
Athozus <athozus@gmail.com>
2023-09-14 20:35:30 +0200

    Reworked settings (#111)
    * Store globally settings (type and default value)
    
    * Add settings groups
    
    * Generate settings pages with global storage
    
    Add saving, generate selection idxs from settings list, order settings via index value in each group of settings
    
    * Rewrite setting store
    
    No code change, only format
    
    Co-authored-by: SX <50966843+S-S-X@users.noreply.github.com>
    
    * Remove tabs from settings groups
    
    Use spaces instead
    
    Co-authored-by: SX <50966843+S-S-X@users.noreply.github.com>
    
    * Use table.copy() minetest api function
    
    * Better formatting for setting tooltip
    
    Using inline instead of multiples lines to add tooltip attached to setting into formspec
    
    Co-authored-by: SX <50966843+S-S-X@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SX <50966843+S-S-X@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- translation
+local S = minetest.get_translator("mail")
+
 mail = {
 	-- version
 	version = 3,
@@ -28,14 +31,7 @@ mail = {
 		filter = {},
 		multipleselection = {},
 		optionstab = {},
-		chat_notifications = {},
-		onjoin_notifications = {},
-		hud_notifications = {},
-		sound_notifications = {},
-		unreadcolorenable = {},
-		cccolorenable = {},
-		trash_move_enable = {},
-		auto_marking_read = {},
+		settings_group = {},
 	},
 
 	colors = {
@@ -51,8 +47,64 @@ mail = {
 		new = "#00F529"
 	},
 
+	settings = {
+		chat_notifications = {
+			type = "bool", default = true, group = "notifications", index = 1,
+			label = S("Chat notifications"), tooltip = S("Receive a message in the chat when there is a new message")
+		},
+		onjoin_notifications = {
+			type = "bool", default = true, group = "notifications", index = 2,
+			label = S("On join notifications"), tooltip = S("Receive a message at login when inbox isn't empty") },
+		hud_notifications = {
+			type = "bool", default = true, group = "notifications", index = 3,
+			label = S("HUD notifications"), tooltip = S("Show an HUD notification when inbox isn't empty")
+		},
+		sound_notifications = {
+			type = "bool", default = true, group = "notifications", index = 4,
+			label = S("Sound notifications"), tooltip = S("Play a sound when there is a new message")
+		},
+		unreadcolorenable = {
+			type = "bool", default = true, group = "message_list", index = 1,
+			label = S("Show unread in different color")
+		},
+		cccolorenable = {
+			type = "bool", default = true, group = "message_list", index = 2,
+			label = S("Show CC/BCC in different color")
+		},
+		defaultsortfield = {
+			type = "index", default = 3, group = "message_list", index = 3,
+			label = S("Default sorting field"), dataset = { S("From/To"), S("Subject"), S("Date") }
+		},
+		defaultsortdirection = {
+			type = "index", default = 1, group = "message_list", index = 4,
+			label = S("Default sorting direction"), dataset = { S("Ascending"), S("Descending") }
+		},
+		trash_move_enable = {
+			type = "bool", default = true, group = "other", index = 1,
+			label = S("Move deleted messages to trash")
+		},
+		auto_marking_read = {
+			type = "bool", default = true, group = "other", index = 2,
+			label = S("Automatic marking read"), tooltip = S("Mark a message as read when opened")
+		},
+		date_format = {
+			type = "string", default = "%Y-%m-%d %X", group = "other", index = 3, label = S("Date format"),
+			dataset = {"%Y-%m-%d %X", "%d/%m/%y %X", "%A %d %B %Y %X"}, format = os.date
+		},
+	},
+
+	settings_groups = {
+		{ name = "notifications", label = S("Notifications")},
+		{ name = "message_list",  label = S("Message list")},
+		{ name = "other",         label = S("Other")}
+	},
+
 	message_drafts = {}
 }
+
+for s, _ in pairs(mail.settings) do
+	mail.selected_idxs[s] = {}
+end
 
 if minetest.get_modpath("default") then
 	mail.theme = default.gui_bg .. default.gui_bg_img
