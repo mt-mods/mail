fd42f008520a9d3d166278cc50655f25f1bee7ff
Athozus <athozus@gmail.com>
2023-12-18 20:21:43 +0100

    Avoid false for default transfer value
    In set_setting, to eventually transfer to another mod. Suggested by S-S-X in #127

--- storage.lua
+++ storage.lua
@@ -399,9 +399,9 @@ end
 
 function mail.get_setting(playername, key)
 	local entry = mail.get_storage_entry(playername)
-	local value = entry.settings[key] == nil
-		and mail.get_setting_default_value(key)
-		or entry.settings[key]
+	local value = (entry.settings[key] == nil
+		and {mail.get_setting_default_value(key)}
+		or {entry.settings[key]})[1]
 
 	if mail.settings[key].sync then -- in case this setting is shared with another mod
 		value = mail.settings[key].sync(playername, key) -- get new value
