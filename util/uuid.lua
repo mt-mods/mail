b04762204eaa4368a5e681e25077cefb7b444732
Athozus <athozus@gmail.com>
2024-04-01 16:34:21 +0200

    Merge duplicated functions between migrate.lua and util/uuid.lua

--- util/uuid.lua
+++ util/uuid.lua
@@ -1,7 +1,7 @@
 -- source: https://gist.github.com/jrus/3197011
 local random = math.random
 
-local function search_box(playername, box, uuid)
+function mail.search_box(playername, box, uuid)
 	local e = mail.get_storage_entry(playername)
 	for _, m in ipairs(e[box]) do
 		if m.id == uuid then
@@ -10,19 +10,29 @@ local function search_box(playername, box, uuid)
 	return false
 end
 
-local function is_uuid_existing(uuid)
+function mail.is_uuid_existing(uuid)
     for _, k in ipairs(mail.storage:get_keys()) do
         if string.sub(k,1,5) == "mail/" then
 			local p = string.sub(k, 6)
 			local result
 			local boxes = {"inbox", "outbox", "drafts", "trash"}
 			for _, b in ipairs(boxes) do
-				result = search_box(p, b, uuid)
+				result = mail.search_box(p, b, uuid)
 				if result then return result end
 			end
 		end
     end
     return false
+end
+
+function mail.are_message_sames(a, b)
+	return a.time == b.time
+	   and a.from == b.from
+	   and a.to == b.to
+	   and a.cc == b.cc
+	   and a.bcc == b.bcc
+	   and a.subject == b.subject
+	   and a.body == b.body
 end
 
 function mail.new_uuid()
@@ -34,6 +44,6 @@ function mail.new_uuid()
                 local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
                 return string.format('%x', v)
             end)
-    until not is_uuid_existing(candidate_uuid)
+    until not mail.is_uuid_existing(candidate_uuid)
     return candidate_uuid
 end
